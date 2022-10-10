c35e526ebdaa67a44fff5a0dc814c513ef8ed0e0
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2022-10-10 19:50:46 +0200

    scripting

--- jsMuffin/ingredients.js
+++ jsMuffin/ingredients.js
@@ -1,19 +1,29 @@
-function showRecipe(recipeId) {
-    console.log(recipeId);
-    let list = document.getElementById("ingredients-list");
+const RECIPES = {
+    recipe1: {
+        ingredients: [{
+            label: "bat",
+            quantity: 4
+        }, {
+            label: "bi",
+            quantity: 1
+        }, {
+            label: "hiru",
+            quantity: 20
+        }]
+    }
+}
 
-    let ingredients = [{
-        label: "bat",
-        quantity: 4
-    }, {
-        label: "bi",
-        quantity: 1
-    }, {
-        label: "hiru",
-        quantity: 20
-    }];
-    
-    ingredients.forEach(ingredient => {
+function showRecipe(recipeId) {
+    const recipe = RECIPES[recipeId];
+
+    const title = document.getElementById("recipeTitleLabel");
+    title.innerText = "asdfasdf";
+
+    const stuck = document.getElementById("stuckInput");
+    console.log(stuck);
+
+    const list = document.getElementById("ingredients-list");
+    recipe.ingredients.forEach(ingredient => {
         const li = document.createElement("li");
         li.innerText = ingredient.label + ": " + ingredient.quantity*2;
         list.appendChild(li);
