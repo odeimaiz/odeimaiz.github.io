cbbaf67ed7e1ab80ea4ec387db549d013d4b0ddd
maiz <maiz@itis.swiss>
2025-03-25 20:17:22 +0100

    dynamic forms

--- src/pages/Rsvp.js
+++ src/pages/Rsvp.js
@@ -1,25 +1,36 @@
+import React, { useState, useEffect } from "react";
 import { useTranslation } from 'react-i18next';
 
+const forms = {
+  en: "https://docs.google.com/forms/d/e/1FAIpQLScRrZUECqzjtTvlWFWTNLh2uoYncMSuL0IPwQna2gpmfYm6Lg/viewform?usp=dialog",
+  eu: "https://docs.google.com/forms/d/e/1FAIpQLScc9bPDvklNntyOzL0wo0GSSyD8P9zVtVMqub3xubGBDxOfsA/viewform?usp=dialog",
+};
+
 const Rsvp = () => {
-  const { t } = useTranslation();
-  /*
-  <iframe
-    title="rsvp_form"
-    src="" // GOOGLE_FORM_EMBED_URL
-    width="100%"
-    height="200"
-    frameBorder="0"
-    marginHeight="0"
-    marginWidth="0"
-  >
-    Loading…
-  </iframe>
-  */
+  const { i18n, t } = useTranslation();
+  const [formSrc, setFormSrc] = useState("");
+
+  useEffect(() => {
+    setFormSrc(forms[i18n.language] || forms.en);
+  }, [i18n.language]);
 
   return (
     <section id="rsvp" className="page">
-      <h2 className="text-2xl font-bold mb-4">Hey</h2>
-      Loading…
+      <p>{t("rsvp.body")}</p>
+      {formSrc && ( // Only render iframe when formSrc is set
+        <iframe
+          key={formSrc} // Forces re-render when src changes
+          src={formSrc}
+          title="rsvp"
+          width="100%"
+          height="400"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+      )}
     </section>
   );
 };
