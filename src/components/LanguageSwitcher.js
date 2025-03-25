81636163faea29b9ca0883265c8134b3b6da37bb
maiz <maiz@itis.swiss>
2025-03-25 10:41:18 +0100

    flags

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -9,8 +9,20 @@ const LanguageSwitcher = () => {
 
   return (
     <div className="language-switcher">
-      <button onClick={() => handleChangeLanguage('en')}>English</button>
-      <button onClick={() => handleChangeLanguage('eu')}>Euskara</button>
+      <button onClick={() => i18n.changeLanguage('eu')} aria-label="Euskara">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Flag_of_the_Basque_Country.svg/960px-Flag_of_the_Basque_Country.svg.png"
+          alt="Basque"
+          style={{ width: '30px', height: 'auto' }}
+        />
+      </button>
+      <button onClick={() => i18n.changeLanguage('en')} aria-label="English">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/330px-Flag_of_the_United_Kingdom_%283-5%29.svg.png"
+          alt="English"
+          style={{ width: '28px', height: 'auto' }}
+        />
+      </button>
     </div>
   );
 };
