1e4aace377e0cf31ecd6adc0f33d7460cb4176ef
maiz <maiz@itis.swiss>
2025-03-26 16:42:17 +0100

    intro

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -5,10 +5,11 @@ const Intro = () => {
 
   return (
     <section className="p-8 flex flex-col justify-center items-center">
-      <h1 className="text-2xl font-bold mb-4">{t('intro.text')}</h1>
-      <p>{t('invitation_body')}</p>
+      <h1 className="text-2xl font-bold mb-4">{t('intro.title')}</h1>
+      <p>{t('intro.body_1')}</p>
+      <p>{t('intro.body_2')}</p>
       <p>{t('date')}</p>
-      <p>{t('location')}</p>
+      <p><a href="https://maps.app.goo.gl/8fs4DW7Aq4jRt2jX7" target="_blank">{t('location')}</a></p>
     </section>
   );
 };
