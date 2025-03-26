178cc1825ed1d1d6cb13f03f05f9ac7b888c2b5f
maiz <maiz@itis.swiss>
2025-03-26 18:36:23 +0100

    minor

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -18,15 +18,15 @@ const LanguageSwitcher = () => {
       </button>
       <button onClick={() => i18n.changeLanguage('en')} aria-label="English">
         <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/330px-Flag_of_the_United_Kingdom_%283-5%29.svg.png"
-          alt="English"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Flag_of_Scotland.svg/320px-Flag_of_Scotland.svg.png"
+          alt="Scotish"
           style={{ width: '28px', height: 'auto' }}
         />
       </button>
       <button onClick={() => i18n.changeLanguage('ch')} aria-label="Hopp Schwiiz">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Flag_of_Switzerland_%28Pantone%29.svg/240px-Flag_of_Switzerland_%28Pantone%29.svg.png"
-          alt="English"
+          alt="Hopp Swchwiiz"
           style={{ width: '18px', height: 'auto' }}
         />
       </button>
