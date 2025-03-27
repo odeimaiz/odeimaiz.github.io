fd7c0874fa10f5189d0e9b4e65704c650170a5a9
maiz <maiz@itis.swiss>
2025-03-27 17:35:10 +0100

    minor

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -8,9 +8,9 @@ const Intro = () => {
       <h1 className="text-2xl font-bold mb-4">{t('intro.title')}</h1>
       <p>{t('intro.body_1')}</p>
       <p>{t('intro.body_2')}</p>
-      <p dangerouslySetInnerHTML={{ __html: t('date') }}></p>
-      <p dangerouslySetInnerHTML={{ __html: t('location') }}></p>
-      <p dangerouslySetInnerHTML={{ __html: t('intro.body_3') }}></p>
+      <p dangerouslySetInnerHTML={{ __html: t('intro.date') }}></p>
+      <p dangerouslySetInnerHTML={{ __html: t('intro.location') }}></p>
+      <p dangerouslySetInnerHTML={{ __html: t('intro.rsvp') }}></p>
     </section>
   );
 };
