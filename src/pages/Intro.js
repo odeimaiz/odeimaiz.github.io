7494410151cc474e4521a4e92881f0d48a33e030
maiz <maiz@itis.swiss>
2025-03-28 12:48:28 +0100

    Fix routing

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 const Intro = () => {
   const { t } = useTranslation();
@@ -8,9 +9,9 @@ const Intro = () => {
       <h1 className="text-2xl font-bold mb-4">{t('intro.title')}</h1>
       <p>{t('intro.body_1')}</p>
       <p>{t('intro.body_2')}</p>
-      <p dangerouslySetInnerHTML={{ __html: t('intro.date') }}></p>
-      <p dangerouslySetInnerHTML={{ __html: t('intro.location') }}></p>
-      <p dangerouslySetInnerHTML={{ __html: t('intro.rsvp') }}></p>
+      <p><Link to="/details">{t('intro.date')}</Link></p>
+      <p><Link to="/location">{t('intro.location')}</Link></p>
+      <p><Link to="/rsvp">{t('intro.rsvp') }</Link></p>
     </section>
   );
 };
